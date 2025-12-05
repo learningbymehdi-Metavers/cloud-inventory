25edd19ae4fa681daf118b3d0a51ba098f9e1e1f
Mehdi-devdev <antifimehdi.m@gmail.com>
2025-12-05 06:47:03 +0100

    Add home route for /

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import export_stock_csv
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("✅ Inventory app is running. Try /export/ or /admin/")
 
 urlpatterns = [
+    path("", home, name="home"),
     path("export/", export_stock_csv, name="export_stock_csv"),
 ]
